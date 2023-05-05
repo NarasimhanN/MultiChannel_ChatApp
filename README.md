8205eaa89bbdf2a30174103d07cd927789f6ebf9
NarasimhanN <nnarasimha.nn@gmail.com>
2023-05-05 17:11:02 +0530

    Final Commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MultiChannel_ChatApp
+
 React-native | Firebase
+
+This is a multi group anonomous chat application build using React-native and Firebase as backend.
+The Chats are End-to-End encrypted using AES Symmetric key encyption.
+
+---
+
+To Start :
+
+In Chat directory
+
+> > npm install
+> > npm start
+
+NOTE :
+Make sure you create a new project in Firebase and set your web credientials in the config.js file
+
+---
--- config.js
+++ config.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 const firebaseConfig = {
-  apiKey: "-",
-  authDomain: "-.firebaseapp.com",
-  projectId: "-",
-  storageBucket: "-..com",
-  messagingSenderId: "",
-  appId: "1:::",
-  measurementId: "G-",
+  apiKey: "AIzaSyCw3twh65YldC-Ba0DlQwhTMbb4GlVa47E",
+  authDomain: "dbdemo-f68b8.firebaseapp.com",
+  projectId: "dbdemo-f68b8",
+  storageBucket: "dbdemo-f68b8.appspot.com",
+  messagingSenderId: "47818523891",
+  appId: "1:47818523891:web:94a5c54d11803c6602a386",
+  measurementId: "G-HWDJLG52DK",
 };
 
 if (!firebase.apps.length) {
